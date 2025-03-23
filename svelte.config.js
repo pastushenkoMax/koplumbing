7df0641cfd0d9cbe1598b6c377a0bcc7c50b2283
pastushenkoMax <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>
2025-03-22 19:05:05 -0600

    test commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,38 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+// export default {
+//   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+//   // for more information about preprocessors
+//   preprocess: vitePreprocess(),
+// }
+
+
+
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+// export default {
+//   kit: {
+//     adapter: adapter(),
+//     paths: {
+//       // base: '/koplumbing'
+//       base: dev ? "" : process.env.BASE_PATH ,
+//     }
+//   }
+// };
+
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
