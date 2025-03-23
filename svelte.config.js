af911a2596cbe9e67c8b6f93ffcf9f3ee612d6a0
pastushenkoMax <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>
2025-03-22 20:24:03 -0600

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,3 @@
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-// export default {
-//   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-//   // for more information about preprocessors
-//   preprocess: vitePreprocess(),
-// }
-
-
-
-// import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-// export default {
-//   kit: {
-//     adapter: adapter(),
-//     paths: {
-//       // base: '/koplumbing'
-//       base: dev ? "" : process.env.BASE_PATH ,
-//     }
-//   }
-// };
-
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -30,7 +7,8 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/koplumbing',
+			assets: process.env.BASE_PATH || '/koplumbing', // Ensure assets are also prefixed with /koplumbing
 		}
 	}
 };
